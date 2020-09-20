bd94cf3d7eff5a333b72655dd2aaebb98e0fc682
annabellej <annabelleju02@gmail.com>
2020-09-19 20:06:37 -0700

    Emptied AdventureState (not needed).

--- src/main/java/student/server/AdventureState.java
+++ src/main/java/student/server/AdventureState.java
@@ -16,8 +16,4 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
  */
 @JsonSerialize
 public class AdventureState {
-    // TODO: Add any additional state your game needs to this object.
-    // E.g.: If your game needs to display a life total, you could add:
-    // private int lifeTotal;
-    // ...and whatever constructor/getters/setters you'd need
 }
